d1e23e7d438426b80993352530452ae25ad75663
Abraão Reis <andradeabraao23@gmail.com>
2025-03-11 15:46:45 -0300

    Index Interface Done

--- components/Header.vue
+++ components/Header.vue
@@ -1,7 +1,32 @@
+<script setup>
+import { ref, watch, onMounted } from 'vue';
+
+const isDark = ref(false);
+
+const toggleTheme = () => {
+    const toggleText = document.querySelector('.toggle-text');
+
+    document.body.classList.toggle('dark', isDark.value);
+
+    if (isDark.value) {
+        toggleText.style.paddingLeft = '18px';
+    } else {
+        toggleText.style.paddingLeft = '44px'
+    }
+}
+</script>
+
 <template>
     <header class="header"> <!--Definindo modo temporariamente-->
         <h1>MSP</h1>
-        <div class="buttontemp"></div>
+        <div class="theme-toggle">
+            <label class="switch">
+                <input type="checkbox" v-model="isDark" @change="toggleTheme">
+                <span class="slider">
+                    <span class="toggle-text" style="display: block;">{{ isDark ? 'ON' : 'OFF' }}</span>
+                </span>
+            </label>
+        </div>
     </header>
 </template>
 
@@ -24,12 +49,76 @@
 
         color: var(--text);
     }
+
+    .switch {
+        position: relative;
+        display: inline-block;
+        width: 100px;
+        height: 40px;
+    }
+
+    .switch input {
+        opacity: 0;
+        width: 0;
+        height: 0;
+    }
     
-    .buttontemp {
+    .slider {
+        display: flex;
+        align-items: center;
+        position: absolute;
+        cursor: pointer;
+        background-color: var(--quaternary);
+        border-radius: 34px;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        transition: 0.4s;
+    }
+
+    .slider::before {
+        content: "";
+        position: absolute;
+        height: 34px;
+        width: 34px;
+        left: 3px;
+        bottom: 3px;
+        background-color: var(--text);
+        border-radius: 50%;
+        transition: 0.4s;
+    }
+
+    input:checked + .slider {
+        background-color: white;
+
+        .toggle-text {
+            color: var(--quaternary);
+        }
+    }
+
+    input:checked + .slider::before {
+        background-color: var(--tertiary);
+
+        transform: translateX(60px);
+    }
+    
+    .theme-toggle {
+        display: flex;
+        justify-content: center;
+        align-items: center;
         height: 40px;
         width: 100px;
-    
-        background-color: white;
+    }
+
+    .toggle-text {
+        padding-left: 44px;
+
+        font-family: 'Montserrat';
+        font-weight: 600;
+        font-size: var(--font-size-input);
+
+        color: white;
     }
 }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,161 @@
 <template>
     <div>
         <Header />
-        <h1>Gestão de Pedidos</h1>
+        <main>
+            <h1>Gestão de Pedidos</h1>
+            <form action="#" method="post">
+                <div>
+                    <label for="iid">ID</label>
+                    <input type="text" name="id" id="iid" placeholder="auto" readonly> <!--Virá automaticamente como número-->
+                </div>
+                <div>
+                    <label for="idate">Data</label>
+                    <input type="text" name="date" id="idate" placeholder="dd/mm/aaaa" maxlength="10"> <!--Optado para melhor controle. Uso de RegEx para máscara de data-->
+                </div>
+                <div>
+                    <label for="iresp">Responsável</label>
+                    <input type="text" name="name" id="iresp" placeholder="Ex.: Agostinho de Hipona" maxlength="11">
+                </div>
+                <div>
+                    <button id="history">HISTÓRICO</button>
+                    <input type="submit" value="CRIAR" id="create">
+                </div>
+            </form>
+            <footer>
+                <p>@MercadinhoSaoPedro</p>
+            </footer>
+        </main>                        
     </div>
 </template>
 
 <style scoped>
-h1 {
-    padding: 20px;
-    
+main {
+    padding: var(--spacing-md);
+
     font-family: 'Montserrat';
-    font-weight: lighter;
+}
+
+main h1 {
+    padding-bottom: var(--spacing-md);
+    
     font-weight: 700;
-    font-size: 36px;
+    font-size: var(--font-size-lg);
     text-align: center;
     text-shadow: 0px 4px 4px rgba(47, 50, 57, 0.15);
     
     color: var(--text);
 }
+
+main form {
+    display: flex;
+    flex-flow: column nowrap;
+    gap: var(--spacing-md);
+    margin: var(--spacing-md) 0;
+
+    font-weight: 600;
+    font-size: var(--font-size-slg);
+    text-align: center;
+    text-shadow: 0px 4px 4px rgba(47, 50, 57, 0.15);
+
+    color: var(--text);
+    
+    div {
+            display: flex;
+            flex-flow: column nowrap;
+            align-items: center;
+            gap: var(--spacing-sm);
+
+            input {
+                height: 53px;
+
+                font-family: 'Montserrat';
+                font-size: var(--font-size-input);
+                text-align: center;
+        
+                background-color: var(--text);
+        
+                border: 0;
+                border-radius: 10px;
+            }
+
+            input::placeholder {
+                text-align: center;
+            }
+
+            input:focus {
+                box-shadow: 0 0 8px rgba(47, 50, 57, 0.6);
+
+                outline: none;
+            }
+        }
+
+
+    div:nth-of-type(1) input {
+        width: 120px;
+    }
+
+    div:nth-of-type(2) input {
+        width: 200px;
+    }
+
+    div:nth-of-type(3) input {
+        width: 346px;
+    }
+
+    div:nth-of-type(4) {
+        display: flex;
+        flex-flow: row;
+        justify-content: center;
+        gap: var(--spacing-md);
+
+        font-size: var(--font-size-base);
+        
+        button {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            
+            height: 53px;
+            width: 162px;
+            
+            font-weight: 300;
+            font-size: var(--font-size-base);
+            
+            color: var(--text);
+            background-color: var(--secondary);
+            
+            border: 0;
+            border-radius: 10px;
+            
+            cursor: pointer;
+        }
+
+        button:focus {
+            box-shadow: 0 0 8px rgba(47, 50, 57, 0.6);
+
+            outline: none;
+        }
+
+        input {
+            width: 162px;
+            
+            font-size: var(--font-size-base);
+            
+            color: var(--text);
+            background-color: var(--primary);
+            
+            cursor: pointer;
+        }
+    }
+}
+
+footer {
+    margin-top: 60px;
+
+    font-size: var(--font-size-base);
+    text-align: center;
+
+    color: var(--text);
+}
+
 </style>
