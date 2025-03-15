26bf99f75b67af5f8db9e844341ba338ea8d8f5f
Abraão Reis <andradeabraao23@gmail.com>
2025-03-15 00:46:54 -0300

    Print Stated: Table and ShareButtons created

---
+++ components/ShareButtons.vue
@@ -0,0 +1,25 @@
+<script setup></script>
+
+<template>
+    <div class="share-buttons">
+        <NuxtLink to="/">
+            <Button variant="quaternary"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 1024 1024"><path fill="currentColor" d="M224 480h640a32 32 0 1 1 0 64H224a32 32 0 0 1 0-64"/><path fill="currentColor" d="m237.248 512l265.408 265.344a32 32 0 0 1-45.312 45.312l-288-288a32 32 0 0 1 0-45.312l288-288a32 32 0 1 1 45.312 45.312z"/></svg></Button>
+        </NuxtLink>
+        <Button variant="quaternary"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M4 16.004V17a3 3 0 0 0 3 3h10a3 3 0 0 0 3-3v-1M12 4.5v11m3.5-3.5L12 15.5L8.5 12"/></svg></Button>
+        <Button variant="quaternary"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24"><path fill="currentColor" d="M16.61 21q-.994 0-1.687-.695q-.692-.696-.692-1.69q0-.15.132-.757l-7.197-4.273q-.324.374-.793.587t-1.007.213q-.986 0-1.676-.702T3 12t.69-1.683t1.676-.702q.537 0 1.007.213t.793.588l7.198-4.255q-.07-.194-.101-.385q-.032-.192-.032-.392q0-.993.697-1.689Q15.625 3 16.62 3t1.688.697T19 5.389t-.695 1.688t-1.69.692q-.542 0-1-.222t-.78-.597l-7.199 4.273q.07.194.101.386q.032.191.032.391t-.032.391t-.1.386l7.198 4.273q.323-.375.78-.597q.458-.222 1-.222q.994 0 1.69.696q.695.698.695 1.693t-.697 1.688t-1.692.692"/></svg></Button>
+    </div>
+</template>
+
+<style scoped>
+.share-buttons {
+    display: flex;
+    flex-flow: row nowrap;
+    gap: var(--spacing-md);
+
+    margin-top: 60px;
+
+    button {
+        width: 110px;
+    }
+}
+</style>
--- pages/print.vue
+++ pages/print.vue
@@ -1,7 +1,163 @@
 <script setup></script>
 
 <template>
-    <p>Aqui ficará a pré-visualização, download e compartilhamento.</p>
+    <div class="table-container">
+        <table>
+            <thead>
+                <tr>
+                    <th>PRODUTO</th>
+                    <th>TIPO</th>
+                    <th>QTD</th>
+                </tr>
+            </thead>
+        </table>
+        <div class="table-body">
+            <table>
+                <tbody>
+                    <tr>
+                        <td>AÇÚCAR DEMERARA KG</td>
+                        <td>SC</td>
+                        <td>002</td>
+                    </tr>
+                    <tr>
+                        <td>FLOCÃO CORINGA 500G</td>
+                        <td>FD</td>
+                        <td>004</td>
+                    </tr>
+                    <tr>
+                        <td>MACAR BANDINE ESPAGUETE</td>
+                        <td>FD</td>
+                        <td>001</td>
+                    </tr>
+                    <tr>
+                        <td>ARROZ URBANO 5KG</td>
+                        <td>UN</td>
+                        <td>005</td>
+                    </tr>
+                    <tr>
+                        <td>ARROZ TIO URBANO KG</td>
+                        <td>FD</td>
+                        <td>002</td>
+                    </tr>
+                    <tr>
+                        <td>VINAGRE MARATÁ 500ML</td>
+                        <td>FD</td>
+                        <td>001</td>
+                    </tr>
+                    <tr>
+                        <td>CAFÉ MARATÁ 250G</td>
+                        <td>FD</td>
+                        <td>004</td>
+                    </tr>
+                    <tr>
+                        <td>SARDINHA 88 ÓLEO</td>
+                        <td>CX</td>
+                        <td>001</td>
+                    </tr>
+                    <tr>
+                        <td>MARG DELÍCIA 250G</td>
+                        <td>FD</td>
+                        <td>004</td>
+                    </tr>
+                    <tr>
+                        <td>BISC RECH AMORI MORANGO 250G</td>
+                        <td>CX</td>
+                        <td>002</td>
+                    </tr>
+                    <tr>
+                        <td>ESPONJA AÇO ASSOLAN</td>
+                        <td>FD</td>
+                        <td>002</td>
+                    </tr>
+                    <tr>
+                        <td>SARDINHA 88 ÓLEO</td>
+                        <td>CX</td>
+                        <td>001</td>
+                    </tr>
+                    <tr>
+                        <td>ESPONJA AÇO ASSOLAN</td>
+                        <td>FD</td>
+                        <td>002</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+    <ShareButtons />
 </template>
 
-<style scoped></style>
+<style scoped>
+.table-container {
+  width: 340px;
+
+  margin-top: var(--spacing-slg);
+  font-family: 'Montserrat';
+  font-weight: bold;
+  font-size: var(--font-size-sm);
+  color: var(--quaternary);
+
+  border-radius: 10px;
+  overflow: hidden;
+}
+
+.table-container > table {
+    table-layout: fixed;
+    width: 100%;
+    border-top-left-radius: 10px;
+    border-top-right-radius: 10px;
+
+    th:nth-of-type(1) {
+        width: 70%;
+    }
+}
+
+.table-body > table {
+    border-bottom-left-radius: 10px;
+}
+
+table {
+  width: 100%;
+  border-collapse: collapse;
+  background-color: var(--text);
+}
+
+th, td {
+  padding: var(--spacing-sm);
+  text-align: left;
+  border-bottom: 1px solid rgba(55, 75, 87, 0.5);
+}
+
+tr:last-of-type td {
+  border-bottom: none;
+}
+
+.table-body {
+  max-height: 360px;
+  overflow-y: auto;
+  overflow-x: hidden;
+}
+
+.table-body table {
+  width: 100%;
+  border-collapse: collapse;
+}
+
+.table-body::-webkit-scrollbar {
+  width: 8px;              /* Largura da barra */
+}
+
+.table-body::-webkit-scrollbar-track {
+  background: #e0e0e0;     /* Fundo da barra */
+}
+
+.table-body::-webkit-scrollbar-thumb {
+  background-color: #888;  /* Cor da "bolinha" da barra */
+  border: 2px solid #e0e0e0;
+}
+
+.table-body::-webkit-scrollbar-thumb:hover {
+  background-color: #555;  /* Cor ao passar o mouse */
+  cursor: grab;
+}
+
+</style>
